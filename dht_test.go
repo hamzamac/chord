1ddd8b0c2ea5aa3ca8f722e62481e7019d2ab8e0
hamzamac <hamzamac@live.com>
2015-10-07 01:04:07 +0200

    test2

--- dht_test.go
+++ dht_test.go
@@ -13,7 +13,7 @@ func TestDHT1(t *testing.T) {
 	id3 := "03"
 	id4 := "04"
 	id5 := "05"
-	//id6 := "06"
+	id6 := "06"
 	id7 := "07"
 
 	node0b := makeDHTNode(&id0, "localhost", "1111")
@@ -22,7 +22,7 @@ func TestDHT1(t *testing.T) {
 	node3b := makeDHTNode(&id3, "localhost", "1114")
 	node4b := makeDHTNode(&id4, "localhost", "1115")
 	node5b := makeDHTNode(&id5, "localhost", "1116")
-	//node6b := makeDHTNode(&id6, "localhost", "1117")
+	node6b := makeDHTNode(&id6, "localhost", "1117")
 	node7b := makeDHTNode(&id7, "localhost", "1118")
 
 	
@@ -33,7 +33,7 @@ func TestDHT1(t *testing.T) {
 	node1b.addToRing(node3b)
 	node1b.addToRing(node4b)
 	node4b.addToRing(node5b)
-	//node3b.addToRing(node6b)
+	node3b.addToRing(node6b)
 	node3b.addToRing(node7b)
 
 	//fmt.Println("-> ring structure")
